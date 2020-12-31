e8d9cf07a6d54348db5e1e01d3ec69d713c9a572
calcium1992 <55957127+calcium1992@users.noreply.github.com>
2020-12-31 00:05:47 -0500

    Try to Commit

--- atari/reinforcement_learning_p2-master/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(
-    name='DeepRL Project 2',
-    version='1.0',
-    description='Library for Deep Reinforcement Learning Project 2',
-    packages=['deeprl_p2'])
